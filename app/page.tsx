96b6471316812a9befb6ff4646f530a6a78085cf
Daniel <>
2024-11-21 21:47:24 +0200

    enabled daisyui

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    Welcome to job diary
+    <h1 className="text-red-600">Welcome to job diary</h1>
     </>
   );
 }
